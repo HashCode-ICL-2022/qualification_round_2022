acaf6eadc2f5b397eaeece1beebc7548c00a734e
RabidSheep55 <tharreaupierre@gmail.com>
2022-02-24 19:19:16 +0000

    export

--- export_file.py
+++ export_file.py
@@ -1,4 +1,8 @@
-def export_file(fname):
+def export(results, fname):
 
-    f = open('outputs/' + fname), "w")
-    f.writelines(string)
+    with open(fname, "w") as file:
+        file.write(str(len(results)) + '\n')
+
+        for res in results:
+            file.write(res[0]['name'] + '\n')
+            file.write(' '.join([person['name'] for person in res[1]]) + "\n")
