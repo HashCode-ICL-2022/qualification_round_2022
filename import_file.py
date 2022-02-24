e34b8bf359054fa6c5a1e1834b8503b576dc6cb2
RabidSheep55 <tharreaupierre@gmail.com>
2022-02-24 18:55:22 +0000

    imp

--- import_file.py
+++ import_file.py
@@ -1,10 +1,36 @@
-def import_file(fname):
-    with open('data/' + fname, 'r') as f:
-        N = int(file.readline())
+def load(file_name):
+    with open(file_name, 'r') as file:
 
-        lines = []
-        for i in range(N):
-            line = file.readline().rstrip().split(" ")
-            lines.append(line)
+        # Read first line
+        C, P = [int(i) for i in file.readline().rstrip().split(' ')]
 
-        return lines
+        print(f"[{file_name}] Loading {C} Contributors and {P} Projects")
+
+        contributors = []
+        for i in tqdm(range(C), desc='Contribs'):
+            name, N = file.readline().rstrip().split(' ')
+            skills = []
+            for j in range(int(N)):
+                s, lvl = file.readline().rstrip().split(' ')
+                skills += [(s, int(lvl))]
+
+            contributors += [dict(name=name, skills=skills)]
+
+        projects = []
+        for i in tqdm(range(P), desc='Projects'):
+            name, D, S, B, R = file.readline().rstrip().split(' ')
+            roles = []
+            for j in range(int(R)):
+                s, lvl = file.readline().rstrip().split(' ')
+                roles += [(s, int(lvl))]
+
+            projects += [
+                dict(name=name,
+                     D=int(D),
+                     S=int(S),
+                     B=int(B),
+                     R=int(R),
+                     roles=roles)
+            ]
+
+        return contributors, projects
